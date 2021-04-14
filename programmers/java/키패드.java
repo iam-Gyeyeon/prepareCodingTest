b285a0f98768d438c3ab74fa6bd9b41d467fcef2
iam-Gyeyeon <82353294+iam-Gyeyeon@users.noreply.github.com>
2021-04-14 09:15:31 +0900

    Create 키패드.java
    2021-04-14 09:15 점심먹고 마저하기

---
+++ programmers/java/키패드.java
@@ -0,0 +1,13 @@
+/*
+판별 해야 될 것
+1. 현재 내 손가락의 위치
+2. 목표 숫자(배열 값)에 왼손이 가까운지 오른손이 가까운지 판별(좌표 이용?)
+3. 그 판별한 값과 배열 안의 값이 일치하면 ? 
+*/
+
+
+class Solution {
+  public static String solution(int[] numbers, String hand){
+      
+  }
+}
